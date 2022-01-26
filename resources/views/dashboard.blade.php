e20ed260c870a7c23b2a2a735c4712e18d8a1e37
LogicSatinn <amigohmccclurkin98@gmail.com>
2022-01-27 00:52:25 +0300

    last refactoring

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,34 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!doctype html>
+<html lang="en">
+@include('layouts.inc.head')
+<body class="horizontal light  ">
+<div class="wrapper">
+    @include('layouts.inc.nav')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+    <main role="main" class="main-content">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="row align-items-center mb-2">
+                        <div class="col">
+                            <h2 class="h5 page-title">Welcome, {{auth()->user()->name}}!</h2>
+                        </div>
+                        <div class="col-auto">
+                            <form class="form-inline">
+                                <div class="form-group d-none d-lg-inline">
+                                    <label for="reportrange" class="sr-only">Date Ranges</label>
+                                    <div id="reportrange" class="px-2 py-2 text-dark-dark">
+                                        <span class="small"></span>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div> <!-- .col-12 -->
+            </div> <!-- .row -->
+        </div> <!-- .container-fluid -->
+        </main> <!-- main -->
+</div> <!-- .wrapper -->
+@include('layouts.inc.scripts')
+</body>
+</html>
